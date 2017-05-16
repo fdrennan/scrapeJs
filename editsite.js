2ae30b325ade1211785a6f70e83fb4ac2eb1197a
Freddy Drennan <drennanfreddy@gmail.com>
2017-05-16 12:56:27 -0500

    First Commit

---
+++ editsite.js
@@ -0,0 +1,10 @@
+// scrape_techstars.js
+var webPage = require('webpage')
+var page = webPage.create();
+var fs = require('fs');
+var path ='website.html'
+page.open('http://www.scoreboard.com/mlb/standings/', function (status) {
+  var content = page.content;
+ fs.write(path,content,'w')
+  phantom.exit();
+});
---
+++ openMe.R
@@ -0,0 +1,14 @@
+# Explanation here: https://www.r-bloggers.com/web-scraping-javascript-rendered-sites/
+
+library(rvest)
+source("scrapeFunction.R")
+
+site = "http://www.scoreboard.com/mlb/standings/" 
+output = "website.html"
+
+scrapeJs(siteName = site, outputName = output)
+
+website = read_html(output) %>% 
+  html_table()
+
+website
